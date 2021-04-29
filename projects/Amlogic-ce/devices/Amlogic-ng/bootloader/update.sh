8f3a19c99d18e9e1ed64a3e8afb3abbf24780c96
RuralHunter <ruralhunter@gmail.com>
2021-04-29 16:47:33 +0800

    use customized dtb file for n1

--- projects/Amlogic-ce/devices/Amlogic-ng/bootloader/update.sh
+++ projects/Amlogic-ce/devices/Amlogic-ng/bootloader/update.sh
@@ -55,6 +55,9 @@ for arg in $(cat /proc/cmdline); do
           *lafrite)
             SUBDEVICE="LaFrite"
             ;;
+          *meson_gxl_s905d_phicomm_n1)
+            SUBDEVICE="Phicomm_N1"
+            ;;
         esac
       fi
 
@@ -188,30 +191,23 @@ if [ -f $BOOT_ROOT/aml_autoscript ]; then
 fi
 
 # Phicomm_N1
-echo "Updating Phicomm_N1 boot files..."
-if [ -f $SYSTEM_ROOT/usr/share/bootloader/s905_autoscript ]; then
-  echo "Updating s905_autoscript..."
-  cp -p $SYSTEM_ROOT/usr/share/bootloader/s905_autoscript $BOOT_ROOT
-  sleep 1
-fi
+if [ "${SUBDEVICE}" == "Phicomm_N1" ]; then
+	echo "Updating Phicomm_N1 boot files..."
+	if [ -f $SYSTEM_ROOT/usr/share/bootloader/s905_autoscript ]; then
+	  echo "Updating s905_autoscript..."
+	  cp -p $SYSTEM_ROOT/usr/share/bootloader/s905_autoscript $BOOT_ROOT
+	  sleep 1
+	fi
 
-if [ -f $SYSTEM_ROOT/usr/share/bootloader/Phicomm_N1_boot.ini ]; then
-  echo "Updating boot.ini with Phicomm_N1_boot.ini..."
-  cp -p $SYSTEM_ROOT/usr/share/bootloader/Phicomm_N1_boot.ini $BOOT_ROOT/boot.ini
-  sed -e "s/@BOOT_UUID@/$BOOT_UUID/" \
-      -e "s/@DISK_UUID@/$DISK_UUID/" \
-      -i $BOOT_ROOT/boot.ini
-  sleep 1
+	[ -z "$UPDATE_DIR" ] && UPDATE_DIR="/storage/.update"
+	if [ -f $UPDATE_DIR/.tmp/*/3rdparty/bootloader/uInitrd ]; then
+	  echo "Updating uInitrd..."
+	  cp $UPDATE_DIR/.tmp/*/3rdparty/bootloader/uInitrd $BOOT_ROOT
+	  sleep 1
+	fi
+	echo "Phicomm_N1 boot files updated."
+	sleep 2
 fi
-
-[ -z "$UPDATE_DIR" ] && UPDATE_DIR="/storage/.update"
-if [ -f $UPDATE_DIR/.tmp/*/3rdparty/bootloader/uInitrd ]; then
-  echo "Updating uInitrd..."
-  cp $UPDATE_DIR/.tmp/*/3rdparty/bootloader/uInitrd $BOOT_ROOT
-  sleep 1
-fi
-echo "Phicomm_N1 boot files updated."
-sleep 2
 
 mount -o ro,remount $BOOT_ROOT
 
--- projects/Amlogic-ce/devices/Amlogic-ng/packages/u-boot-Phicomm_N1/package.mk
+++ projects/Amlogic-ce/devices/Amlogic-ng/packages/u-boot-Phicomm_N1/package.mk
@@ -5,9 +5,28 @@ PKG_DEPENDS_TARGET="toolchain u-boot-tools-aml u-boot-script"
 PKG_DEPENDS_UNPACK="linux"
 PKG_TOOLCHAIN="manual"
 
-makeinstall_target() {
-  mkdir -p $INSTALL/usr/share/bootloader
+DT_ID="meson_gxl_s905d_phicomm_n1"
+
+make_target() {
+  # Enter kernel directory
+  pushd $BUILD/build/linux-$(kernel_version) > /dev/null
   
+  cp -av $PKG_DIR/sources/$DT_ID.dts arch/$TARGET_KERNEL_ARCH/boot/dts/amlogic/coreelec-gxl/
+  
+  # Compile device trees
+  kernel_make coreelec-gxl/$DT_ID.dtb
+  cp arch/$TARGET_KERNEL_ARCH/boot/dts/amlogic/coreelec-gxl/$DT_ID.dtb $PKG_BUILD
+  
+  popd > /dev/null
+}
+
+makeinstall_target() {
   # Install dtb file
-  cp -av $BUILD/build/linux-$(kernel_version)/arch/$TARGET_KERNEL_ARCH/boot/dts/amlogic/coreelec-gxl/gxl_p230_2g.dtb $INSTALL/usr/share/bootloader/Phicomm_N1_dtb.img
+  mkdir -p $INSTALL/usr/share/bootloader  
+  #cp -av $BUILD/build/linux-$(kernel_version)/arch/$TARGET_KERNEL_ARCH/boot/dts/amlogic/coreelec-gxl/gxl_p230_2g.dtb $INSTALL/usr/share/bootloader/Phicomm_N1_dtb.img
+  cp -av $BUILD/build/linux-$(kernel_version)/arch/$TARGET_KERNEL_ARCH/boot/dts/amlogic/coreelec-gxl/$DT_ID.dtb $INSTALL/usr/share/bootloader/Phicomm_N1_dtb.img
+  
+  # For update
+  mkdir -p $INSTALL/usr/share/bootloader/device_trees
+  cp -av $BUILD/build/linux-$(kernel_version)/arch/$TARGET_KERNEL_ARCH/boot/dts/amlogic/coreelec-gxl/$DT_ID.dtb $INSTALL/usr/share/bootloader/device_trees/
 }
